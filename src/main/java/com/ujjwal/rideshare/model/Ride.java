a054af69369d5f59898a30cbe78add77b139da88
Ujjwal <ujjwalsahu19@gmail.com>
2025-12-07 19:20:20 +0530

    model creation

---
+++ src/main/java/com/ujjwal/rideshare/model/Ride.java
@@ -0,0 +1,25 @@
+package com.ujjwal.rideshare.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "rides")
+public class Ride {
+    
+    @Id
+    private String id;
+    private String userId;
+    private String driverId;
+    private String pickupLocation;
+    private String dropLocation;
+    private String status;
+    private Date createdAt;
+}
